8db754e92c1ec619080c5a54ee750357504a4e48
valencaceres <vcaceres102@gmail.com>
2024-12-29 23:16:40 -0300

    login fixed

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -10,17 +10,20 @@ import { useUser } from "./store/hooks";
 function App() {
   const { user } = useUser();
   console.log(user);
+  
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<PrivateRoute/>}>
+        <Route element={<PrivateRoute />}>
           <Route path="/" element={<Layout />}>
-            <Route path="/" element={<Home />} />
+            <Route index element={<Navigate to="/login" />} />
+            <Route path="home" element={<Home />} /> 
             <Route path="edit/:id" element={<Edit />} />
           </Route>
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="sign-up" element={<SignUp />} />
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );
